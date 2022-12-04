d2478476adb188c020814bba963c1813371df190
Duco <duco@dutop-xps.fritz.box>
2022-12-04 14:22:26 +0100

    AOC day 4

---
+++ 2022/day_4/solution1.py
@@ -0,0 +1,15 @@
+f = open("input", "r")
+
+lines = f.readlines()
+total = 0
+nums = list()
+
+for x in lines:
+	s = x.split(",")
+	nums.append([s[0].split("-"), s[1].split("-")])
+
+for x in nums:
+	if ((int(x[0][0]) >= int(x[1][0]) and int(x[0][1]) <= int(x[1][1]))
+	or (int(x[0][0]) <= int(x[1][0]) and int(x[0][1]) >= int(x[1][1]))):
+		total += 1
+print(total)
